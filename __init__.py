3152e031ced3411891b425d20c50f41d124dc213
jmartin <jmartin@zikzakmedia.com>
2014-10-13 10:43:14 +0200

    pep8

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-#This file is part party_event module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of party_event module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.pool import Pool
 from .party_event import *
 from .party import *
+
 
 def register():
     Pool.register(
