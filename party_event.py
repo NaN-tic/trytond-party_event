73dbdcd9fa4b23287176cff6d3aa89da4a271fd0
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:27:39 +0100

    Cleanup tests

--- party_event.py
+++ party_event.py
@@ -1,6 +1,6 @@
-#This file is part party_event module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part party_event module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond import backend
 from trytond.model import ModelView, ModelSQL, fields
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part party_event module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part party_event module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_party_event import suite
 
